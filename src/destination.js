0c6fe7e1113917e05d33fc454a89626c1c213ef5
soewaiyanag <soewaiyanag@gmail.com>
2021-12-16 13:39:43 +0630

    finished destination for mobile

--- src/destination.js
+++ src/destination.js
@@ -1,2 +1,50 @@
 import "./style.css";
 import "./shared_script";
+
+const planetPic = document.getElementById("planet-pic");
+const planetName = document.getElementById("planet-name");
+const aboutPlanet = document.getElementById("about-planet");
+const travelDistance = document.getElementById("travel-distance");
+const travelTime = document.getElementById("travel-time");
+const tabs = document.querySelectorAll("[data-tab]");
+
+let requestURL = "./data.json";
+let request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+
+request.onload = () => {
+  const data = request.response;
+  const planetData = data.destinations;
+
+  showDatas(planetData, 0);
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      removeBorders(tabs);
+      addBorder(tab);
+      showDatas(planetData, index);
+    });
+  });
+};
+
+function showDatas(data, index) {
+  planetPic.src = data[index].images.png ?? "error";
+  planetName.innerText = data[index].name ?? "error";
+  aboutPlanet.innerText = data[index].description ?? "error";
+  travelDistance.innerText = data[index].distance ?? "error";
+  travelTime.innerText = data[index].travel ?? "error";
+}
+
+function removeBorders(els) {
+  els.forEach((el) => {
+    el.classList.remove("border-b-2");
+    el.classList.remove("border-white");
+  });
+}
+
+function addBorder(el) {
+  el.classList.add("border-b-2");
+  el.classList.add("border-white");
+}
